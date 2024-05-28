045dcc2c298ae2ee8e12c04f74594c524929c74c
vrma <vrmachado@gmail.com>
2024-05-28 12:39:52 +0200

    sufijo f para asignar un valor a un float

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -9,5 +9,19 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello Mundo!!" );
+        
+        // Tipo de datos primitivo: No tiene propiedades ni metodos
+        int a = 10;
+        
+        // Los tipos de datos objetos, que son un envoltorio de los tipos primitivos
+        // si tienen propiedades y metodos. Por cada tipo primitivo existe un tipo objeto
+        // correspondiente.
+        Integer b = 20;
+        
+        // Para almacenar un valor en un float, hay especificar un sufijo (f o F) porque de lo contrario
+        // consider el valor como un double, y un double no se puede almacenar en un float
+        float c = 2.3f;
+        
+        
     }
 }
